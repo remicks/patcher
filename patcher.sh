a071b3690567052690cbad7e4e7677272957a867
remicks <lost.junglist@gmail.com>
2016-03-13 11:11:51 -0700

    Remove lock clock patch

--- patcher.sh
+++ patcher.sh
@@ -31,13 +31,6 @@ wget -q https://github.com/sultanxda/android_frameworks_base/commit/52e9a4337ef2
 patch -p1 -s < 52e9a4337ef23a02e1f5fde16f6241a490d32a85.patch
 git clean -f -d
 
-# packages/apps/LockClock:
-cd ../../packages/apps/LockClock
-git reset --hard && git clean -f -d
-wget -q https://github.com/sultanxda/android_packages_apps_LockClock/commit/87427a68afeef2d2ab13750b24c88db6b88786dd.patch
-patch -p1 -s < 87427a68afeef2d2ab13750b24c88db6b88786dd.patch
-git clean -f -d
-
 # packages/apps/Trebuchet:
 cd ../../../packages/apps/Trebuchet
 git reset --hard && git clean -f -d
